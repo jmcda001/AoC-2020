9951a43bdc2f51fd185a3cbf753c7eda35259560
Jeffrey-laptop <jmcda001@ucr.edu>
2020-12-01 14:05:40 -0800

    remove input files

--- days/src/Main.kt
+++ days/src/Main.kt
@@ -2,5 +2,6 @@ import day1.Day1
 
 fun main() {
     val evaluateDay = { day: Day -> DayResults(day.puzzle1(), day.puzzle2())}
+
     print("Day 1: ${evaluateDay(Day1.buildFromFile("day1/input1.txt"))}")
 }
