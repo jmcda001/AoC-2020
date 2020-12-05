2f1764f2a85630172a4574077480d96dcc73e9f1
Jeffrey-laptop <jmcda001@ucr.edu>
2020-12-04 17:05:24 -0800

    solve both puzzles

--- days/src/day4/Day4.kt
+++ days/src/day4/Day4.kt
@@ -4,44 +4,58 @@ import Day
 import java.io.File
 
 class Day4(private val passports: List<Map<String, String>>) : Day {
-    private fun Map<String, String>.isValidPassport(): Boolean {
-        val requiredFields = listOf("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")//, "cid")
-        val optional = listOf("cid")
-        return requiredFields.all { field -> field in this }
+    enum class PassportField {
+        BYR, IYR, EYR, HGT, HCL, ECL, PID;//, CID
+
+        override fun toString() = super.toString().toLowerCase()
+        private val _eyeColors = listOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
+        fun validate(value: String) = when (this) {
+            BYR -> value.toInt() in 1920..2002
+            IYR -> value.toInt() in 2010..2020
+            EYR -> value.toInt() in 2020..2030
+            HGT -> "(\\d+)(in|cm)".toRegex().matchEntire(value)?.let {
+                when (it.groupValues[2]) {
+                    "in" -> it.groupValues[1].toInt() in 59..76
+                    "cm" -> it.groupValues[1].toInt() in 150..193
+                    else -> false
+                }
+            } ?: false
+            ECL -> value in _eyeColors
+            HCL -> "#[0-9a-f]{6}".toRegex().matches(value)
+            PID -> "\\d{9}".toRegex().matches(value)
+        }
+    }
+
+    private fun Map<String, String>.isValidPassport(useStrict: Boolean = false): Boolean {
+        return PassportField.values().all { field ->
+            field.toString() in this && (!useStrict || field.validate(this[field.toString()]!!))
+        }
     }
 
     override fun puzzle1(): Int? = passports.count { it.isValidPassport() }
-    override fun puzzle2(): Int? = null
+    override fun puzzle2(): Int? = passports.count { it.isValidPassport(useStrict = true) }
 
     companion object {
+        private fun String.buildPassport() = this.trim().split(' ').associate { keyValue ->
+            keyValue.split(':').let {
+                Pair(it[0].trim(), it[1])
+            }
+        }
+
         fun buildFromFile(filename: String): Day4 {
-            var currentCombined: String = ""
+            var currentCombined = ""
             val passports = mutableListOf<Map<String, String>>()
-            var count = 0
-            val fields = listOf("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid")
             File(filename).useLines { lines ->
                 lines.forEach { line ->
                     if (line.isEmpty()) {
-                        val keys = currentCombined.trim().split(' ').map {kv ->
-                            kv.split(':').let { it[0].trim() }
-                        }
-                        if (keys.size != keys.distinct().size || keys.all { it in fields }) {
-                            println("$keys (${keys.size}:${keys.distinct().size}")
-                        }
-                        if (keys.size == 8 || (keys.size == 7 && !keys.contains("cid"))) {
-                            count++
-                        }
-                        passports.add(currentCombined.trim().split(' ').associate { keyValue ->
-                            keyValue.split(':').let {
-                                Pair(it[0].trim(), it[1]) }
-                        })
+                        passports.add(currentCombined.buildPassport())
                         currentCombined = ""
                     } else {
-                        currentCombined += line + " "
+                        currentCombined += "$line "
                     }
                 }
             }
-            println("${passports.size} passports to process, $count truly valid")
+            passports.add(currentCombined.buildPassport())
             return Day4(passports)
         }
     }
