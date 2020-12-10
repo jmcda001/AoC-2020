e41c25005bc147d28281d59bfa738be9575256e3
Jeffrey-laptop <jmcda001@ucr.edu>
2020-12-10 07:02:05 -0800

    solve both puzzles on day 10

--- days/src/Main.kt
+++ days/src/Main.kt
@@ -1,4 +1,5 @@
 import day1.Day1
+import day10.Day10
 import day2.Day2
 import day3.Day3
 import day4.Day4
@@ -10,14 +11,25 @@ import day9.Day9
 
 fun main() {
     val evaluateDay = { day: Day -> DayResults(day.puzzle1(), day.puzzle2())}
+    val days = listOf(
+            {fn: String -> evaluateDay(Day1.buildFromFile(fn))},
+            {fn: String -> evaluateDay(Day2.buildFromFile(fn))},
+            {fn: String -> evaluateDay(Day3.buildFromFile(fn))},
+            {fn: String -> evaluateDay(Day4.buildFromFile(fn))},
+            {fn: String -> evaluateDay(Day5.buildFromFile(fn))},
+            {fn: String -> evaluateDay(Day6.buildFromFile(fn))},
+            {fn: String -> evaluateDay(Day7.buildFromFile(fn))},
+            {fn: String -> evaluateDay(Day8.buildFromFile(fn))},
+            {fn: String -> evaluateDay(Day9.buildFromFile(fn))},
+            {fn: String -> evaluateDay(Day10.buildFromFile(fn))}
+    )
 
-    println("Day 1: ${evaluateDay(Day1.buildFromFile("day1/input.txt"))}")
-    println("Day 2: ${evaluateDay(Day2.buildFromFile("day2/input.txt"))}")
-    println("Day 3: ${evaluateDay(Day3.buildFromFile("day3/input.txt"))}")
-    println("Day 4: ${evaluateDay(Day4.buildFromFile("day4/input.txt"))}")
-    println("Day 5: ${evaluateDay(Day5.buildFromFile("day5/input.txt"))}")
-    println("Day 6: ${evaluateDay(Day6.buildFromFile("day6/input.txt"))}")
-    println("Day 7: ${evaluateDay(Day7.buildFromFile("day7/input.txt"))}")
-    println("Day 8: ${evaluateDay(Day8.buildFromFile("day8/input.txt"))}")
-    println("Day 9: ${evaluateDay(Day9.buildFromFile("day9/input.txt"))}")
+    val runAll = false
+    if (runAll) {
+        days.forEachIndexed { i, day ->
+            println("Day ${i + 1}: ${day("day${i + 1}/input.txt")}")
+        }
+    } else {
+        println("Day ${days.size}: ${days.last()("day${days.size}/input.txt")}")
+    }
 }
